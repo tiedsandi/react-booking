a2d5d1aa6570fe146cd10a310e0f936c824d6703
tiedSandi <wkwkwkand@gmail.com>
2023-05-31 01:15:42 +0700

    routing + add sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+
+const router = createBrowserRouter([
+  { path: "*", Component: Root },
+]);
 
 const App = () => {
+  return <RouterProvider router={router} />;
+}
+
+function Root() {
+
   return (
-    <div>Home</div>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/hotels" element={<List />} />
+      <Route path="/hotels/:id" element={<List />} />
+    </Routes>
+  );
 }
 
 export default App
