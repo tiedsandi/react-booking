98eec7197323cbf6fc8fa9f77953c9e55acddd45
tiedSandi <wkwkwkand@gmail.com>
2023-06-05 08:17:54 +0700

    detail hotels page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
 import {
-  createBrowserRouter,
-  Route,
-  RouterProvider,
-  Routes,
+	createBrowserRouter,
+	Route,
+	RouterProvider,
+	Routes
 } from "react-router-dom";
 
 import Home from "./pages/home/home.component";
 import List from "./pages/list/list.component";
 
-import './App.style.scss'
+import "./App.style.scss";
+import Hotel from "./pages/hotel/hotel.component";
 
-
-const router = createBrowserRouter([
-  { path: "*", Component: Root },
-]);
+const router = createBrowserRouter([{ path: "*", Component: Root }]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
-}
+	return <RouterProvider router={router} />;
+};
 
 function Root() {
-
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/hotels" element={<List />} />
-      <Route path="/hotels/:id" element={<List />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/hotels" element={<List />} />
+			<Route path="/hotels/:id" element={<Hotel />} />
+		</Routes>
+	);
 }
 
-export default App
+export default App;
